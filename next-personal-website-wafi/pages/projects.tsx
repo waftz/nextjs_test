9009bf3779d7667b5cbcc75d28e5c919f78c480f
Wafiamna <wafiamna08@gmail.com>
2025-07-23 11:13:45 +0700

    Belajar column grid

--- next-personal-website-wafi/pages/projects.tsx
+++ next-personal-website-wafi/pages/projects.tsx
@@ -14,6 +14,20 @@ export default function Projects() {
     </header>
     <div className="mt-16">
       <h2 className="text-2xl">Apps</h2>
+      <ul className="grid grid-cols-4 gap-x-12 gap-y-16 mt-8">
+        <li>
+          <a href="[link]" className="href">
+            <div className="max-w-sm rounded overflow-hidden shadow-lg">
+              <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">[name]</div>
+              </div>
+              <div className="px-6 pb-4">
+                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">🔗 [URL]</span>
+              </div>
+            </div>
+          </a>
+        </li>
+      </ul>
     </div>
     <div className="mt-16 mb-8">
       <h2 className="text-2xl">Books</h2>
